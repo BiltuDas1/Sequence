b48acca65b40809748e189cd696a813341ce91a0
BiltuDas1 <billionto@gmail.com>
2026-06-25 23:00:50 +0530

    build: Released beta version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.github.biltudas1.sequence"
         minSdk = 24
         targetSdk = 37
-        versionCode = 31
-        versionName = "0.14.0a9"
+        versionCode = 32
+        versionName = "0.14.0b1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
